23abe35a6dfdc4ba8375360d512a1fb2cce67229
maxfridbe <maxfridbe@gmail.com>
2026-05-11 17:57:43 -0500

    fix: correct turbo type_size/blck_size in ggml.c (turbo3: blck=128 size=50, turbo2: blck=128 size=34, turbo4: blck=128 size=68)

--- ggml/src/ggml.c
+++ ggml/src/ggml.c
@@ -1946,11 +1946,11 @@ static const ggml_type_traits_t type_traits[GGML_TYPE_COUNT] = {
         .nrows                    = 1,
         .row_meta_size            = 0,
     },
-    [GGML_TYPE_TURBO2_0] = { .type_name = "TURBO2_0", .blck_size = 32, .type_size = 18, .is_quantized = true, .to_float = NULL, .from_float = NULL, .from_float_ref = NULL, .vec_dot = NULL, .vec_dot_type = GGML_TYPE_F16, .nrows = 1 },
-    [GGML_TYPE_TURBO3_0] = { .type_name = "TURBO3_0", .blck_size = 32, .type_size = 20, .is_quantized = true, .to_float = NULL, .from_float = NULL, .from_float_ref = NULL, .vec_dot = NULL, .vec_dot_type = GGML_TYPE_F16, .nrows = 1 },
-    [GGML_TYPE_TURBO4_0] = { .type_name = "TURBO4_0", .blck_size = 32, .type_size = 24, .is_quantized = true, .to_float = NULL, .from_float = NULL, .from_float_ref = NULL, .vec_dot = NULL, .vec_dot_type = GGML_TYPE_F16, .nrows = 1 },
-    [GGML_TYPE_TQ3_1S]   = { .type_name = "TQ3_1S",   .blck_size = 32, .type_size = 20, .is_quantized = true, .to_float = NULL, .from_float = NULL, .from_float_ref = NULL, .vec_dot = NULL, .vec_dot_type = GGML_TYPE_F16, .nrows = 1 },
-    [GGML_TYPE_TQ4_1S]   = { .type_name = "TQ4_1S",   .blck_size = 32, .type_size = 24, .is_quantized = true, .to_float = NULL, .from_float = NULL, .from_float_ref = NULL, .vec_dot = NULL, .vec_dot_type = GGML_TYPE_F16, .nrows = 1 },
+    [GGML_TYPE_TURBO2_0] = { .type_name = "TURBO2_0", .blck_size = 128, .type_size = 34, .is_quantized = true, .to_float = NULL, .from_float = NULL, .from_float_ref = NULL, .vec_dot = NULL, .vec_dot_type = GGML_TYPE_F16, .nrows = 1 },
+    [GGML_TYPE_TURBO3_0] = { .type_name = "TURBO3_0", .blck_size = 128, .type_size = 50, .is_quantized = true, .to_float = NULL, .from_float = NULL, .from_float_ref = NULL, .vec_dot = NULL, .vec_dot_type = GGML_TYPE_F16, .nrows = 1 },
+    [GGML_TYPE_TURBO4_0] = { .type_name = "TURBO4_0", .blck_size = 128, .type_size = 68, .is_quantized = true, .to_float = NULL, .from_float = NULL, .from_float_ref = NULL, .vec_dot = NULL, .vec_dot_type = GGML_TYPE_F16, .nrows = 1 },
+    [GGML_TYPE_TQ3_1S]   = { .type_name = "TQ3_1S",   .blck_size = 32,  .type_size = 20, .is_quantized = true, .to_float = NULL, .from_float = NULL, .from_float_ref = NULL, .vec_dot = NULL, .vec_dot_type = GGML_TYPE_F16, .nrows = 1 },
+    [GGML_TYPE_TQ4_1S]   = { .type_name = "TQ4_1S",   .blck_size = 32,  .type_size = 24, .is_quantized = true, .to_float = NULL, .from_float = NULL, .from_float_ref = NULL, .vec_dot = NULL, .vec_dot_type = GGML_TYPE_F16, .nrows = 1 },
 };
 
 // For internal test use
